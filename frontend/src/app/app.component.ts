345557b19b1bf4b802516f452aac421a2aea0817
Luyn <xxxtrglyn@gmail.com>
2024-01-17 21:55:36 +0700

    initial commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './shared/layouts/layout/layout.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, LayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'frontend';
+}
